c2bf1572e5e415b509f204788886ca67d74abc74
zallowh <160600988+zallowh@users.noreply.github.com>
2025-07-04 16:10:31 +0200

    Update layout.js
    google gsc

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/footer";
 
+<meta name="google-site-verification" content="OaiF7KTgvKv92jCi09phUANThw4wLYBIphOX5wuE7GU" />
+
 export const metadata = {
   title: "GetStrongerMe | Business Websites & SEO",
   description: "Affordable websites, SEO, and hosting for small businesses in Johannesburg.",
